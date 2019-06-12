e5f701da50b7b3675d9df958da1e31a4ac4391a4
matgaspar <matheusgaspar13@hotmail.com>
2019-06-12 13:52:00 -0300

    Update: Feed concluído + services/api

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,10 +1,81 @@
 import React, { Component } from 'react';
+import api from '../services/api';
 
-class Feed extends Component{
-    render(){
+import './Feed.css';
+
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
+
+class Feed extends Component {
+    state = {
+        feed: [],
+    }
+    async componentDidMount() {
+        const response = await api.get("posts");
+
+        this.setState({ feed: response.data });
+    }
+    render() {
         return (
-            <h1>Feed</h1>
-        )
+            <section id="post-list">
+                { this.state.feed.map(post => (
+                    <article key={post._id}>
+                        <header>
+                            <div className="user-info">
+                                <span>{post.author}</span>
+                                <span className="place">{post.place}</span>
+                            </div>
+
+                            <img src={more} alt="Mais" />
+                        </header>
+
+                        <img src={`http://localhost:3333/files/${post.image}`} alt="" />
+
+                        <footer>
+                            <div className="actions">
+                                <img src={like} alt="" />
+                                <img src={comment} alt="" />
+                                <img src={send} alt="" />
+                            </div>
+                            <strong>{post.likes} curtidas</strong>
+
+                            <p>
+                                {post.description}
+                            <span>{post.hashtags}</span>
+                            </p>
+                        </footer>
+                    </article>
+                )) }
+                {/* <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Matheus Gaspar</span>
+                            <span className="place">Piraí / Rio de Janeiro</span>
+                        </div>
+
+                        <img src={more} alt="Mais"/>
+                    </header>
+
+                    <img src="http://localhost:3333/files/rn_bg.jpg" alt="" />
+
+                    <footer>
+                        <div className="actions">
+                        <img src={like} alt=""/>
+                        <img src={comment} alt=""/>
+                        <img src={send} alt=""/>
+                        </div>
+                        <strong>900 curtidas</strong>                            
+
+                        <p>
+                            Um post muito massa da Omnistack!
+                            <span>#react #omnistack #top</span>
+                        </p>
+                    </footer>
+                </article> */}
+            </section>
+        );
     }
 }
 
